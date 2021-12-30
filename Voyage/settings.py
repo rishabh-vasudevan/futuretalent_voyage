c13d8af0b5a480a4a463416154d98cf5c279f80b
rishabh-vasudevan <rishabhvasudevan19@gmail.com>
2021-12-31 02:27:13 +0530

    new commit

--- Voyage/settings.py
+++ Voyage/settings.py
@@ -137,9 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
